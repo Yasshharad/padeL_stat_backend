74591f4505ae1487ca037d553676358df4af05ea
Yasshharad <it.a.35.yasshharad@gmail.com>
2024-04-20 21:11:26 +0530

    changed

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/api/v1/padel-match-stat', padelMatchStatRoutes);
 app.use(errorMiddleware);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
