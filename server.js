bde400e714c6990ced6ffebe7d6e98ae6f96ea46
Yasshharad <it.a.35.yasshharad@gmail.com>
2024-04-21 11:29:11 +0530

    mongo edited

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 googleAuthConfig(passport);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
