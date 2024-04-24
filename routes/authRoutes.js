85e608f916872a60505bfd8b2f73bc90094cbe38
Yasshharad <it.a.35.yasshharad@gmail.com>
2024-04-24 20:17:55 +0530

    changes done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,8 +13,12 @@ router.get("/google/callback",passport.authenticate("google",{
 
 router.get("/login/success", async (req, res) => {
     if (req.user) {
-        res.status(200).json({ message: "user Login", user: req.user });
-    } else {
+        res.status(200).json({
+          success: true,
+          message: "successfull",
+          user: req.user
+        });
+      } else {
         res.status(400).json({ message: "Not Authorized" });
     }
 });
